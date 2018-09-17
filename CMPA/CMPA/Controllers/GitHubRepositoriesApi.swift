d809bb66187e154641ce250280db5681f9c3f955
MortenLiebmann <morten_liebmann@hotmail.com>
2018-09-17 21:07:10 +0200

    Added Pods and basic framework

---
+++ CMPA/CMPA/Controllers/GitHubRepositoriesApi.swift
@@ -0,0 +1,9 @@
+//
+//  GitHubRepositoriesApi.swift
+//  CMPA
+//
+//  Created by Morten Liebmann Andersen on 17/09/2018.
+//  Copyright © 2018 Morten Liebmann Andersen. All rights reserved.
+//
+
+import Foundation
---
+++ CMPA/CMPA/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  CMPA
+//
+//  Created by Morten Liebmann Andersen on 17/09/2018.
+//  Copyright © 2018 Morten Liebmann Andersen. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var Id: String?
+    var Repos_Url: String?
+    
+    
+    
+}
