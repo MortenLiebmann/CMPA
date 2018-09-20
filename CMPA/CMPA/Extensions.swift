38abaea9755663ee1e315d243c5a8b59b4b1c0c4
MortenLiebmann <morten_liebmann@hotmail.com>
2018-09-20 18:56:15 +0200

    Added an extension for UIImageView to download images

---
+++ CMPA/CMPA/Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  Extensions.swift
+//  CMPA
+//
+//  Created by Morten Liebmann Andersen on 20/09/2018.
+//  Copyright © 2018 Morten Liebmann Andersen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func downloadImage(from urlString: String?) {
+        guard let urlString = urlString, let url = URL(string: urlString) else {
+            self.image = nil
+            return
+        }
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            guard let data = data, let image = UIImage(data: data) else {
+                self.image = nil
+                return
+                
+            }
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+        }
+        
+        task.resume()
+    }
+}
